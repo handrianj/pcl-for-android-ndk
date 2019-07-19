1b50794c9eeec8fe308e71fee0daf73603e68b29
Heri Andrianjafy <heri@coolhobo.com>
2019-07-19 11:46:37 +0800

    uncomment the download for boost and flan

--- download-setup.sh
+++ download-setup.sh
@@ -19,7 +19,7 @@ tar -jxf 3.2.9.tar.bz2 -C ${build}/eigen --strip-components 1 || exit 1
 echo -e "\n\n\033[1;35m###########################################"
 echo -e "### FLANN download and setup            ###"
 echo -e "###########################################\033[m\n\n"
-#[ -f ${flann} ] || wget http://www.cs.ubc.ca/research/flann/uploads/FLANN/${flann} -O ${flann} || exit 1
+[ -f ${flann} ] || wget http://www.cs.ubc.ca/research/flann/uploads/FLANN/${flann} -O ${flann} || exit 1
 echo "extracting ${flann} to ${build}/flann ..."
 unzip -o -q flann-1.8.4-src.zip || exit 1
 mv flann-1.8.4-src ${build}/flann || exit 1
@@ -27,7 +27,7 @@ mv flann-1.8.4-src ${build}/flann || exit 1
 echo -e "\n\n\033[1;35m###########################################"
 echo -e "### BOOST download and setup            ###"
 echo -e "###########################################\033[m\n\n"
-#[ -f ${boost} ] || wget https://sourceforge.net/projects/boost/files/boost/1.61.0/${boost} -O ${boost} || exit 1
+[ -f ${boost} ] || wget https://sourceforge.net/projects/boost/files/boost/1.61.0/${boost} -O ${boost} || exit 1
 mkdir ${build}/boost
 echo "extracting ${boost} to ${build}/boost ..."
 tar -jxf boost_1_61_0.tar.bz2 -C ${build}/boost --strip-components 1 || exit 1
